65906b42785d29dba1ac3dee02daa591085e47e4
Ashleysa <ashleysa2001@gmail.com>
2021-07-11 21:39:47 -0400

    Agregamos el resto de los archivos, bibliotecas e imagenes usados en el proyecto

---
+++ js/clase_4.js
@@ -0,0 +1,60 @@
+/*document.getElementById("demo").innerHTML = 5 + 6;
+
+document.write(5 + 6);
+
+window.alert(25 + 5);
+
+alert(52 + 12);
+
+console.log(56 - 12);
+
+function myFunction(){
+	document.getElementById("demo1").innerHTML = "Hello Dolly";
+	document.getElementById("demo2").innerHTML = "How are you?";
+}
+
+var	x, y, z;
+var x = 5; var y = 12; var z = -6;
+
+let a = 5; 
+let b = 12;
+let c = 9;
+
+let n1 = 3.14152957;
+
+let nombre = "Ashley";
+let apellido = "de Herrera";*/
+
+/* let a = 5;
+let b = 6;
+let c = 10;
+console.log("La suma de 5 + 6 es: " + (a + b) + " y la multiplicacion de a + b * c es: " + (a + b) * c);
+ 
+
+console.log(5*10);
+
+console.log("Jhon" + " " + "Doe"); // Jhon Doe
+	var last_name = "Vilchez";
+	var lastname = "Herrera";
+
+	const PI = 3.141529; // Constante se declaran en mayusculas
+	const LOG = 2.71; 
+
+ esta funcion me permitira sumar dos numeros enteros
+console.log("La suma de " + 45 + " " + 23 + " es: " + (45+23));
+*/
+
+// declaramos la variables
+var x = 12;
+var y = 25;
+var z = x + y;
+
+console.log(z);
+
+var precio_1 = 56;
+var precio_2 = 69;
+var total = precio_1 + precio_2;
+console.log("El precio total es: " + total + " BsS");
+
+var carName = "Volvo";
+console.log("El nombre del carro es: " + carName);
---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Esta es una funcion 
+/* Este es un comentario
+de varias lineas*/
+
+
+	function myFunction(){
+		document.getElementById("demo").innerHTML="Mi primer parrafo en Javascript";
+	}
